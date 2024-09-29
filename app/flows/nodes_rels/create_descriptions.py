0406f2926b0e790eaf053faba478f785a560e037
Nikola <nikolovski.nikola42@gmail.com>
2024-09-30 00:24:35 +0200

    added try catch for creating descriptions

--- app/flows/nodes_rels/create_descriptions.py
+++ app/flows/nodes_rels/create_descriptions.py
@@ -11,38 +11,43 @@ mdb = MongoDBDatabase()
 
 chunks = mdb.get_entries(class_type=Chunk)
 
-for chunk in tqdm(chunks[:250], desc="Create node descriptions"):
-    triplets = mdb.get_entries(
-        class_type=Triplet,
-        collection_name="RelUpdatedTriplet",
-        doc_filter={"chunk_id": chunk.id}
-    )
-    nodes = set()
-    if len(triplets) > 0:
-        for triplet in triplets:
-            nodes.add(triplet.head_value)
-            nodes.add(triplet.tail_value)
-
-        name_descr_dict = create_description_chain(
-            terms=nodes,
-            context=chunk.context
+for chunk in tqdm(chunks[33:250], desc="Create node descriptions"):
+    try:
+        triplets = mdb.get_entries(
+            class_type=Triplet,
+            collection_name="RelUpdatedTriplet",
+            doc_filter={"chunk_id": chunk.id}
         )
-        new_nodes:Dict[str, Node] = {}
-        for name, descr in name_descr_dict.items():
-            new_node = Node(
-                id=str(uuid.uuid4()),
-                name=name,
-                description=descr,
+        nodes = set()
+        if len(triplets) > 0:
+            for triplet in triplets:
+                nodes.add(triplet.head_value)
+                nodes.add(triplet.tail_value)
+
+            name_descr_dict = create_description_chain(
+                terms=nodes,
+                context=chunk.context
             )
+            new_nodes:Dict[str, Node] = {}
+            for name, descr in name_descr_dict.items():
+                new_node = Node(
+                    id=str(uuid.uuid4()),
+                    name=name,
+                    description=descr,
+                )
 
-            new_nodes[name] = new_node
+                new_nodes[name] = new_node
 
-            mdb.add_entry(entity=new_node, collection_name="NewNode")
+                mdb.add_entry(entity=new_node, collection_name="NewNode")
 
-        for triplet in triplets:
-            head_node = new_nodes[triplet.head_value]
-            tail_node = new_nodes[triplet.tail_value]
-            triplet.head_id = head_node.id
-            triplet.tail_id = tail_node.id
-            mdb.update_entity(entity=triplet, collection_name="RelUpdatedTriplet")
+            for triplet in triplets:
+                head_node = new_nodes[triplet.head_value]
+                tail_node = new_nodes[triplet.tail_value]
+                triplet.head_id = head_node.id
+                triplet.tail_id = tail_node.id
+                mdb.update_entity(entity=triplet, collection_name="RelUpdatedTriplet")
+
+    except Exception as e:
+        print("Problem creating node descriptions")
+        continue
 
